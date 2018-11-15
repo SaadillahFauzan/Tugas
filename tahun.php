2f7267cd85164a9849d8b2a554664e7483fd48f0
SaadillahFauzan <43674480+SaadillahFauzan@users.noreply.github.com>
2018-11-15 07:36:48 +0700

    Add files via upload

---
+++ tahun.php
@@ -0,0 +1,30 @@
+<?php
+$bulan = $_POST["a"];
+$tahun = $_POST["b"];
+switch($bulan){
+	case 1:
+	case 2:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+
+		$hari = 31;
+		break;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		$hari = 30;
+		break;
+	case 2:
+		if (($tahun%4)== 0){
+			$hari = 29;
+		}else{
+			$hari = 28;
+	}
+}
+echo("<h2>Jumlah hari pada bulan $bulan tahun $tahun = $hari hari</h2>");
+?>
